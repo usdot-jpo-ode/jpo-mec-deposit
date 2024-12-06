1e8d51fdcf97bdfb7fb176ae9bc1994a04d64cfe
Michael7371 <40476797+Michael7371@users.noreply.github.com>
2024-12-06 10:48:27 -0700

    fixing main application class

--- jpo-mec-deposit/src/main/java/us/dot/its/jpo/ode/mec/deposit/MecDepositApplication.java
+++ jpo-mec-deposit/src/main/java/us/dot/its/jpo/ode/mec/deposit/MecDepositApplication.java
@@ -1,13 +1,33 @@
 package us.dot.its.jpo.ode.mec.deposit;
 
+import java.lang.management.ManagementFactory;
+import javax.management.InstanceAlreadyExistsException;
+import javax.management.MBeanRegistrationException;
+import javax.management.MBeanServer;
+import javax.management.MalformedObjectNameException;
+import javax.management.NotCompliantMBeanException;
+import javax.management.ObjectName;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
+@EnableConfigurationProperties(DepositorProperties.class)
 public class MecDepositApplication {
+	static final int DEFAULT_NO_THREADS = 10;
+	static final String DEFAULT_SCHEMA = "default";
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MalformedObjectNameException, InterruptedException,
+			InstanceAlreadyExistsException, MBeanRegistrationException, NotCompliantMBeanException {
+
 		SpringApplication.run(MecDepositApplication.class, args);
+		MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+		SystemConfig mBean = new SystemConfig(DEFAULT_NO_THREADS, DEFAULT_SCHEMA);
+		ObjectName name = new ObjectName("us.dot.its.jpo.ode.mec.deposit:type=SystemConfig");
+		mbs.registerMBean(mBean, name);
 	}
 
 }
