c3246294893060e9c798e988b2bcbef95992be23
Michael7371 <40476797+Michael7371@users.noreply.github.com>
2024-12-20 23:26:44 -0700

    fixes for CI

---
+++ jpo-mec-deposit/src/test/java/us/dot/its/jpo/ode/mec/deposit/MecDepositApplicationTest.java
@@ -0,0 +1,13 @@
+// package us.dot.its.jpo.ode.mec.deposit;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class MecDepositApplicationTest {
+
+// @Test
+// void contextLoads() {
+// }
+
+// }
