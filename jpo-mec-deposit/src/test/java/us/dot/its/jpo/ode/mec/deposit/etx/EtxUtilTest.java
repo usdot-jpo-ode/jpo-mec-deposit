ca99dd69a0929e399b3abc519fd6cb5c23b496ca
Michael7371 <40476797+Michael7371@users.noreply.github.com>
2025-01-09 10:25:21 -0700

    fix missing import

--- jpo-mec-deposit/src/test/java/us/dot/its/jpo/ode/mec/deposit/etx/EtxUtilTest.java
+++ jpo-mec-deposit/src/test/java/us/dot/its/jpo/ode/mec/deposit/etx/EtxUtilTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import javax.net.ssl.SSLSocketFactory;
